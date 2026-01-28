124ebdfcabb713b6a6c1ba3b6af1200303c40517
ALungFish <laptopofzach@gmail.com>
2026-01-27 17:08:49 -0700

    I hate boots Ch2. Ls.4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode,LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple(self):
@@ -28,3 +28,18 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.value, "hello")
         self.assertEqual(node.children, children)
         self.assertEqual(node.props, {"class": "text"})
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
